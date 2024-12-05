af49e76e87eef1097d49b1ca8bf9cbc90e520f32
sfarantos <spyrosfarantos@gmail.com>
2024-12-05 10:00:46 +0200

    property //

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { OwnerComponent } from "./pages/owner/owner.component";
 import { AboutUsComponent } from "./pages/about-us/about-us.component";
 import { RepairComponent } from "./pages/repair/repair.component";
-import { PropertyComponent } from "./pages/property/property.component";
+// import { PropertyComponent } from "./pages/property/property.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule, RepairComponent, PropertyComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule, RepairComponent,   OwnerComponent], //PropertyComponent ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/property/property.component.ts
+++ src/app/pages/property/property.component.ts
@@ -1,83 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { PropertyService } from '../../service/propertyService.service';
-import { HeaderComponent } from "../../Shared/header/header.component";
-import { FooterComponent } from "../../Shared/footer/footer.component";
-import { CommonModule } from '@angular/common';
-import { Property } from '../../domain/property';
-import { Router } from '@angular/router';
-import { OwnerService } from '../../service/owner-service.service';
-import { PropertyrepairService } from '../../service/property-repair.service';
-import { RepairComponent } from '../repair/repair.component';
-import { Repair } from '../../domain/repair';
+// import { Component, OnInit } from '@angular/core';
+// import { PropertyService } from '../../service/propertyService.service';
+// import { HeaderComponent } from "../../Shared/header/header.component";
+// import { FooterComponent } from "../../Shared/footer/footer.component";
+// import { CommonModule } from '@angular/common';
+// import { Property } from '../../domain/property';
+// import { Router } from '@angular/router';
+// import { OwnerService } from '../../service/owner-service.service';
+// import { PropertyrepairService } from '../../service/property-repair.service';
+// import { RepairComponent } from '../repair/repair.component';
+// import { Repair } from '../../domain/repair';
 
 
-@Component({
-  selector: 'app-property',
-  imports: [HeaderComponent, FooterComponent,CommonModule],
-  templateUrl: './property.component.html',
-  styleUrl: './property.component.scss'
-})
-export class PropertyComponent  implements OnInit {
+// @Component({
+//   selector: 'app-property',
+//   imports: [HeaderComponent, FooterComponent,CommonModule],
+//   templateUrl: './property.component.html',
+//   styleUrl: './property.component.scss'
+// })
+// export class PropertyComponent  implements OnInit {
 
-  properties: any[] = []; 
-  repairs: any[] = []; 
-  selectedPropertyId: number | null = null;
+//   properties: any[] = []; 
+//   repairs: any[] = []; 
+//   selectedPropertyId: number | null = null;
 
   
-  constructor(private router:Router, private propertyService: PropertyService) {}
+//   constructor(private router:Router, private propertyService: PropertyService) {}
 
-  ngOnInit() {
-    this.loadProperties();
-  }
+//   ngOnInit() {
+//     this.loadProperties();
+//   }
 
-  loadProperties() {
-    this.propertyService.getAllProperties().subscribe((data: any[]) => {
-      this.properties = data;
-      console.log(data);
-  });
-  }
+//   loadProperties() {
+//     this.propertyService.getAllProperties().subscribe((data: any[]) => {
+//       this.properties = data;
+//       console.log(data);
+//   });
+//   }
 
-  viewPropertyRepairs(propertyId: number) {
-    this.selectedPropertyId = propertyId;
+//   viewPropertyRepairs(propertyId: number) {
+//     this.selectedPropertyId = propertyId;
 
-    this.propertyService.getRepairsByPropertyId(propertyId).subscribe({
-      next: (repairs: any[]) => {
-        this.repairs = repairs;
-        console.log('Repairs for property ID:', propertyId, repairs);
+//     this.propertyService.getRepairsByPropertyId(propertyId).subscribe({
+//       next: (repairs: any[]) => {
+//         this.repairs = repairs;
+//         console.log('Repairs for property ID:', propertyId, repairs);
       
-  }
-});
-  }
+//   }
+// });
+//   }
 
 
-  createProperty() {
-      this.propertyService.createProperty().subscribe(
-        () => {
-          this.loadProperties();
-        });
-    }
-  }
+//   createProperty() {
+//       this.propertyService.createProperty().subscribe(
+//         () => {
+//           this.loadProperties();
+//         });
+//     }
+//   }
 
-  editProperty(propertyId: number): void {
-    this.router.navigate([`/properties/${propertyId}/update`]);
-  }
+//   editProperty(propertyId: number): void {
+//     this.router.navigate([`/properties/${propertyId}/update`]);
+//   }
 
-  deleteProperty(propertyId: number) {
-    this.propertyService.deleteProperty(propertyId).subscribe(
-      () => this.loadProperties(),
-      (error: any) => console.error('Error deleting property:', error)
-    );
-  }
+//   deleteProperty(propertyId: number) {
+//     this.propertyService.deleteProperty(propertyId).subscribe(
+//       () => this.loadProperties(),
+//       (error: any) => console.error('Error deleting property:', error)
+//     );
+//   }
 
   
 
 
-  searchProperties() {
-      this.propertyService.searchProperties(this.searchCriteria).subscribe(
-        (data: any[]) => this.properties = data,
-        (error: any) => console.error('Error searching properties:', error)
-      );
-  }
+//   searchProperties() {
+//       this.propertyService.searchProperties(this.searchCriteria).subscribe(
+//         (data: any[]) => this.properties = data,
+//         (error: any) => console.error('Error searching properties:', error)
+//       );
+//   }
 
 
 
