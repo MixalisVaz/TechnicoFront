9a5cbd5330a33bd08b9c2c9161764bb9b3e624b1
pplati <plati.panagiota@gmail.com>
2024-12-11 11:05:54 +0200

    Made the Carousel

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  images: string[] = [
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg'
+  ];
 
+  currentIndex: number = 0;
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
