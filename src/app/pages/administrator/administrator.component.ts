ff97dd1f7b3fbd3565cd4cef12e62e1b74112f5d
xtkaravas <xtkaravas@ote.gr>
2024-12-13 11:58:14 +0200

    admin page carousel with updated buttons

--- src/app/pages/administrator/administrator.component.ts
+++ src/app/pages/administrator/administrator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,29 @@ import { Router } from '@angular/router';
   templateUrl: './administrator.component.html',
   styleUrls: ['./administrator.component.scss']
 })
-export class AdministratorComponent {
+export class AdministratorComponent implements OnInit {
+  images: string[] = [
+    'assets/Living20Room_BridgeSt-06_V2_web.jpg',
+    'assets/assets/Ap3m4SRPcE2YwhtrMQ6bh3.jpg',
+    'assets/reath.jpg'
+  ];
+  currentImageIndex = 0;
+
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    setInterval(() => this.nextImage(), 3000);
+  }
 
   navigateTo(page: string) {
     this.router.navigate([`/${page}`]);
+  }
+
+  nextImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
+
+  previousImage() {
+    this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
   }
 }
