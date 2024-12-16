24dd7378d31f51a961fadb9fb4ed8f87ed97958c
xtkaravas <xtkaravas@ote.gr>
2024-12-16 13:21:51 +0200

    comment out snow effect

--- src/app/Shared/snow-effect/snow-effect.component.ts
+++ src/app/Shared/snow-effect/snow-effect.component.ts
@@ -1,60 +1,60 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { tsParticles } from 'tsparticles-engine';
+// import { Component, AfterViewInit } from '@angular/core';
+// import { tsParticles } from 'tsparticles-engine';
 
-@Component({
-  selector: 'app-snow-effect',
-  templateUrl: './snow-effect.component.html',
-  styleUrls: ['./snow-effect.component.scss'],
-})
-export class SnowEffectComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    console.log('Initializing tsParticles...');
+// @Component({
+//   selector: 'app-snow-effect',
+//   templateUrl: './snow-effect.component.html',
+//   styleUrls: ['./snow-effect.component.scss'],
+// })
+// export class SnowEffectComponent implements AfterViewInit {
+//   ngAfterViewInit(): void {
+//     console.log('Initializing tsParticles...');
 
-    tsParticles
-      .load('tsparticles', {
-        particles: {
-          number: {
-            value: 300, 
-          },
-          color: {
-            value: '#ffffff', 
-          },
-          shape: {
-            type: 'circle', 
-          },
-          opacity: {
-            value: 0.8,
-            random: true,
-          },
-          size: {
-            value: 20,
-            random: true,
-          },
-          move: {
-            enable: true,
-            speed: 1, 
-            direction: 'bottom',
-            random: false,
-            straight: false,
-          },
-        },
-          interactivity: {
-            events: {
-              onHover: {
-                enable: true,
-                mode: 'repulse', 
-              },
-            },
-          },
-          background: {
-            color: {
-              value: 'transparent', 
-            },
-          },
-        })
-      .then(() => console.log('tsParticles loaded successfully!'))
-      .catch((error) => console.error('tsParticles failed to load:', error));
-      console.log('Canvas container:', document.getElementById('tsparticles'));
+//     tsParticles
+//       .load('tsparticles', {
+//         particles: {
+//           number: {
+//             value: 300, 
+//           },
+//           color: {
+//             value: '#ffffff', 
+//           },
+//           shape: {
+//             type: 'circle', 
+//           },
+//           opacity: {
+//             value: 0.8,
+//             random: true,
+//           },
+//           size: {
+//             value: 20,
+//             random: true,
+//           },
+//           move: {
+//             enable: true,
+//             speed: 1, 
+//             direction: 'bottom',
+//             random: false,
+//             straight: false,
+//           },
+//         },
+//           interactivity: {
+//             events: {
+//               onHover: {
+//                 enable: true,
+//                 mode: 'repulse', 
+//               },
+//             },
+//           },
+//           background: {
+//             color: {
+//               value: 'transparent', 
+//             },
+//           },
+//         })
+//       .then(() => console.log('tsParticles loaded successfully!'))
+//       .catch((error) => console.error('tsParticles failed to load:', error));
+//       console.log('Canvas container:', document.getElementById('tsparticles'));
 
-  }
-}
+//   }
+// }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Shared/footer/footer.component';
 import { HeaderComponent } from "./Shared/header/header.component";
 import { ReactiveFormsModule } from '@angular/forms';
-import { SnowEffectComponent } from './Shared/snow-effect/snow-effect.component';
+// import { SnowEffectComponent } from './Shared/snow-effect/snow-effect.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule,SnowEffectComponent,],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule,],//SnowEffectComponent,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
