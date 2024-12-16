1beb6aa81d9aa6e7476b270e68ba2ae93b420352
xtkaravas <xtkaravas@ote.gr>
2024-12-16 12:58:59 +0200

    snow effect implementing (needs work)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Shared/footer/footer.component';
 import { HeaderComponent } from "./Shared/header/header.component";
 import { ReactiveFormsModule } from '@angular/forms';
-import { OwnerComponent } from "./pages/owner/owner.component";
-import { AboutUsComponent } from "./pages/about-us/about-us.component";
-import { RepairComponent } from "./pages/repair/repair.component";
-import { PropertyComponent } from "./pages/property/property.component";
-import { HomepageComponent } from "./pages/homepage/homepage.component";
-// import { PropertyComponent } from "./pages/property/property.component";
+import { SnowEffectComponent } from './Shared/snow-effect/snow-effect.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule,], //PropertyComponent ,
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, ReactiveFormsModule,SnowEffectComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
